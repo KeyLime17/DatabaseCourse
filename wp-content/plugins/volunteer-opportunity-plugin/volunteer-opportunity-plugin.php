859adc167b0127eef31f0a1c73a33df4b9988ef2
KeyLime17 <nesterenka.makar@gmail.com>
2025-01-26 14:46:57 -0500

    admin menu & page

--- wp-content/plugins/volunteer-opportunity-plugin/volunteer-opportunity-plugin.php
+++ wp-content/plugins/volunteer-opportunity-plugin/volunteer-opportunity-plugin.php
@@ -6,6 +6,8 @@ Version: 1.0
 Author: Makar Nestsiarenka
 */
 
+
+//Handle installation and uninstallation
 register_activation_hook( __FILE__, 'volunteer_opportunity_plugin_activate');
 function volunteer_opportunity_plugin_activate() {
     global $wpdb;
@@ -34,5 +36,50 @@ function volunteer_plugin_deactivate() {
     global $wpdb;
     $table_name = $wpdb->prefix . 'volunteer_opportunities';
     $wpdb->query("TRUNCATE TABLE $table_name");
+}
+
+//Admin Menu
+
+add_action('admin_menu', 'volunteer_opportunity_plugin_menu');
+function volunteer_opportunity_plugin_menu() {
+    add_menu_page(
+        'Volunteer Opportunities',
+        'Volunteer Opportunities',
+        'manage_options',
+        'volunteer-opportunities',
+        'volunteer_opportunity_plugin_admin_page',
+        'dashicons-groups',
+        20
+    );
+}
+
+function volunteer_opportunity_plugin_admin_page() {
+    global $wpdb;
+    $table_name = $wpdb->prefix . 'volunteer_opportunities';
+
+    if ($_POST['ACTION'] == 'ADD_VOLUNTEER'0 {
+        $position = sanitize_text_field($_POST['position']);
+        $organization = sanitize_text_field($_POST['organization']);
+        $type = sanitize_text_field($_POST['type']);
+        $email = sanitize_text_field($_POST['email']);
+        $description = sanitize_text_field($_POST['description']);
+        $location = sanitize_text_field($_POST['location']);
+        $hours = sanitize_text_field($_POST['hours']);
+        $skills_required = sanitize_text_field($_POST['skills_required']);
+
+        $wpdb->insert(
+            $table_name,
+            array(
+                'position' => $position,
+                'organization' => $organization,
+                'type' => $type,
+                'email' => $email,
+                'description' => $description,
+                'location' => $location,
+                'hours' => $hours,
+                'skills_required' => $skills_required
+            )
+        );
+    })
 }
 ?>
